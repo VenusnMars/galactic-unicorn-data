24107262a31a7c030566098669989739547c3bec
Florian Rieder <florian.rieder@unil.ch>
2026-06-15 09:01:31 +0200

    feat(lib): add line drawing algorithms

---
+++ data/lib/aaline.lua
@@ -0,0 +1,98 @@
+-- Xiaolin Wu's line algorithm
+-- Draw an antialiased line between two points
+-- see https://en.wikipedia.org/wiki/Xiaolin_Wu%27s_line_algorithm
+
+-- fractional part of x
+local function fpart(x)
+  return x - math.floor(x)
+end
+
+local function rfpart(x)
+  return 1 - fpart(x)
+end
+
+local function aa_line(x0, y0, x1, y1, rgb_color)
+  local steep = math.abs(y1 - y0) > math.abs(x1 - x0)
+
+  -- Steep: swap x and y to draw by row
+  if steep then
+    x0, y0 = y0, x0
+    x1, y1 = y1, x1
+  end
+
+  -- Make sure points are correctly ordered
+  if x0 > x1 then
+    x0, x1 = x1, x0
+    y0, y1 = y1, y0
+  end
+
+  -- m = (y1 - y0) / (x1 - x0)
+  local slope = (y1 - y0) / (x1 - x0)
+
+  -- handle first endpoint
+  local xend = math.floor(x0)
+  local yend = y0 + slope * (xend - x0)
+  local xgap = 1 - (x0 - xend)
+  local xpxl1 = xend -- this will be used in the main loop
+  local ypxl1 = math.floor(yend)
+
+  -- Draw the first endpoint
+  if steep then
+      set_pixel_blend(ypxl1, xpxl1, rgb_color, rfpart(yend) * xgap)
+      set_pixel_blend(ypxl1+1, xpxl1, rgb_color, fpart(yend) * xgap)
+  else
+      set_pixel_blend(xpxl1, ypxl1, rgb_color, rfpart(yend) * xgap)
+      set_pixel_blend(xpxl1, ypxl1+1, rgb_color, fpart(yend) * xgap)
+  end
+
+  local intery = yend + slope -- first y-intersection for the main loop
+  
+  -- handle second endpoint
+  xend = math.ceil(x1)
+  yend = y1 + slope * (xend - x1)
+  xgap = 1 - (xend - x1)
+  local xpxl2 = xend -- this will be used in the main loop
+  local ypxl2 = math.floor(yend)
+
+  -- Draw the second endpoint
+  if steep then
+      set_pixel_blend(ypxl2, xpxl2, rgb_color, rfpart(yend) * xgap)
+      set_pixel_blend(ypxl2+1, xpxl2, rgb_color, fpart(yend) * xgap)
+  else
+      set_pixel_blend(xpxl2, ypxl2, rgb_color, rfpart(yend) * xgap)
+      set_pixel_blend(xpxl2, ypxl2+1, rgb_color, fpart(yend) * xgap)
+  end
+
+  -- main loop
+  local lower, upper, frac
+  for i = xpxl1 + 1, xpxl2 - 1 do
+    lower = math.floor(intery)
+    upper = lower + 1
+    frac = intery - lower
+
+    if steep then
+      set_pixel_blend(lower, i, rgb_color, rfpart(intery))
+      set_pixel_blend(upper, i, rgb_color, fpart(intery))
+    else
+      set_pixel_blend(i, lower, rgb_color, rfpart(intery))
+      set_pixel_blend(i, upper, rgb_color, fpart(intery))
+    end
+    intery = intery + slope
+  end
+
+end
+
+if (...) == nil then
+  -- Just a regular line
+  aa_line(0, 0, 12, 4, rgb(255, 0, 0))
+  -- Second point larger than first
+  aa_line(12, 4, 19, 0, rgb(0, 0, 255))
+  -- Steep slope
+  aa_line(0, 0, 2, 9, rgb(0, 255, 0))
+  -- Steep slope with second point larger than first
+  aa_line(2, 9, 3, 0, rgb(255, 255, 0))
+  -- With float coordinates
+  aa_line(0.5, 4.1, 17.3, 8.6, rgb(255, 255, 255))
+end
+
+return aa_line
---
+++ data/lib/line.lua
@@ -0,0 +1,38 @@
+-- Bresenham's line algorithm
+-- Draw a line between two points
+-- see https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+
+local function line(x0, y0, x1, y1, rgb_color)
+  local dx = math.abs(x1 - x0);
+  local dy = math.abs(y1 - y0);
+  local sx = x0 < x1 and 1 or -1;
+  local sy = y0 < y1 and 1 or -1;
+  local err = dx - dy;
+
+  while (true) do
+    set_pixel(x0, y0, rgb_color);
+
+    if x0 == x1 and y0 == y1 then break end
+
+    local e2 = 2 * err;
+
+    if (e2 > -dy) then
+      err = err - dy;
+      x0 = x0 + sx;
+    end
+
+    if (e2 < dx) then
+      err = err + dx;
+      y0 = y0 + sy;
+    end
+  end
+end
+
+if (...) == nil then
+  line(0, 0, 19, 9, rgb(255, 0, 0))
+  line(0, 9, 19, 0, rgb(0, 0, 255))
+  line(0, 0, 2, 9, rgb(0, 255, 0))
+  line(3, 9, 4, 0, rgb(255, 255, 0))
+end
+
+return line
